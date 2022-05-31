518fa2cd14631188c0463f27528685da35b99c21
Nursultan Amanturov <nursultan.amanturov.kk@gmail.com>
2022-05-31 15:50:48 +0600

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+//! DOM  document object model
+//! selektor ....get
+// let btn = document.getElementById("btn");
+// console.log(btn)
+//! querySelectors
+// document.querySelector("#btn");
+
+// let container = document.getElementById("container");
+
+// container.innerText = "Hello DOM!"
+// console.log(container);
+
+
+// let container = document.getElementById("container");
+// for (let i = 1; i <= 10; i++) {
+//     let block = document.createElement("div");
+//     block.style.width = "100px";
+//     block.style.height = "100px";
+//     block.style.backgroundColor = "green";
+//     block.style.margin = "10px";
+//     container.append(block);
+// }
+// container.style.display = "flex";
+// console.log(container.firstChild)
+// console.log(container.firstChild.nextElementSibling)
